d72c32e96f2ee413df125645cffeac4c9305e258
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-02-24 11:52:38 +0600

    add model

--- src/main/java/com/tz/payment_api/model/Payment.java
+++ src/main/java/com/tz/payment_api/model/Payment.java
@@ -1,4 +1,25 @@
 package com.tz.payment_api.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "payments")
 public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "sender_account")
+    private String senderAccount;
+
+    @Column(name = "recipient_account")
+    private String recipientAccount;
+
+    @Column(name = "amount")
+    private BigDecimal amount;
+
+
 }
