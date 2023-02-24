1fbf427d13347b26042919edeb98329766d3359a
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-02-24 17:30:52 +0600

    add service interface

---
+++ src/main/java/com/tz/payment_api/service/PaymentService.java
@@ -0,0 +1,16 @@
+package com.tz.payment_api.service;
+
+import com.tz.payment_api.dto.PaymentRequestDto;
+import com.tz.payment_api.model.Payment;
+
+public interface PaymentService {
+
+    Payment createPayment(PaymentRequestDto paymentRequestDto);
+
+
+    Payment getPaymentById(Long id);
+
+    Payment confirmPayment(Long id);
+
+    Payment cancelPayment(Long id);
+}
