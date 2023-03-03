bd2943ff375d795907e871e76ea378ea6c7a6dc3
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-03-03 20:04:57 +0600

    add Service model

--- src/main/java/com/tz/payment_api/model/Service.java
+++ src/main/java/com/tz/payment_api/model/Service.java
@@ -2,6 +2,8 @@ package com.tz.payment_api.model;
 
 import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+
 @Entity
 @Table(name = "services")
 public class Service {
@@ -13,5 +15,41 @@ public class Service {
     @Column(name = "name")
     private String name;
 
+    @Column(name = "description")
+    private String description;
 
+    @Column(name = "min_amount")
+    private BigDecimal minAmount;
+
+    @Column(name = "req_mask")
+    private String requisiteMask;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private ServiceCategory category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getMinAmount() {
+        return minAmount;
+    }
+
+    public ServiceCategory getCategory() {
+        return category;
+    }
+
+    public String getRequisiteMask() {
+        return requisiteMask;
+    }
 }
+
