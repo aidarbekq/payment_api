24bf6b692a11124461ec4c1e185adc027bfc47f1
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-03-04 19:21:48 +0600

    add repository for category

---
+++ src/main/java/com/tz/payment_api/repository/UslugaCategoryRepository.java
@@ -0,0 +1,13 @@
+package com.tz.payment_api.repository;
+
+import com.tz.payment_api.model.UslugaCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UslugaCategoryRepository extends JpaRepository<UslugaCategory, Long> {
+
+    List<UslugaCategory> findByParentCategory(UslugaCategory parentCategory);
+}
