fe2382bc7c959076c054abd4faf36ad6846ff155
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-03-04 19:56:40 +0600

    undo comments and modify

--- src/main/java/com/tz/payment_api/services/DataInitializationService.java
+++ src/main/java/com/tz/payment_api/services/DataInitializationService.java
@@ -26,14 +26,16 @@ public class DataInitializationService {
     public void init() {
 
         UslugaCategory allCategory = new UslugaCategory("Все", null);
-        UslugaCategory callsCategory = new UslugaCategory("Звонки", allCategory);
-        UslugaCategory internetCategory = new UslugaCategory("Интернет", allCategory);
+        UslugaCategory mainCategory = new UslugaCategory("Связь", null);
+        UslugaCategory callsCategory = new UslugaCategory("Звонки", mainCategory);
+        UslugaCategory internetCategory = new UslugaCategory("Интернет", mainCategory);
         UslugaCategory entertainmentCategory = new UslugaCategory("Развлечения", allCategory);
         UslugaCategory cinemaCategory = new UslugaCategory("Кино", entertainmentCategory);
         UslugaCategory gamesCategory = new UslugaCategory("Игры", entertainmentCategory);
         UslugaCategory roamingCategory = new UslugaCategory("Роуминг", allCategory);
 
         uslugaCategoryRepository.save(allCategory);
+        uslugaCategoryRepository.save(mainCategory);
         uslugaCategoryRepository.save(callsCategory);
         uslugaCategoryRepository.save(internetCategory);
         uslugaCategoryRepository.save(entertainmentCategory);
@@ -43,23 +45,31 @@ public class DataInitializationService {
 
         Usluga callPackage60 = new Usluga("Пакет 60 минут", "new ser", new BigDecimal(200),
                 "12335647", callsCategory);
-//        Usluga callBlackList = new Usluga("Черный список", "black list fun",callsCategory, BigDecimal.valueOf(50));
-//        Usluga callPackage30 = new Usluga("Пакет 30 минут","new ser", callsCategory, BigDecimal.valueOf(100));
-//
-//        Usluga cinemaTicket = new Usluga("Кинотеатр","new ser", cinemaCategory, BigDecimal.valueOf(500));
-//
-//        Usluga roamingPackage20GB = new Usluga("Пакет 20 ГБ", "new ser",roamingCategory, BigDecimal.valueOf(1000));
-//
-//        Usluga internetPackage10GB = new Usluga("Пакет 10 ГБ","new ser", internetCategory, BigDecimal.valueOf(200));
-//        Usluga internetPackage30GB = new Usluga("Пакет 30 ГБ", "new ser",internetCategory, BigDecimal.valueOf(550));
+
+        Usluga callBlackList = new Usluga("Черный список", "black list fun", new BigDecimal(50),
+        "12335647", callsCategory);
+
+        Usluga callPackage30 = new Usluga("Пакет 30 минут","new ser",
+                new BigDecimal(100), "12335647", callsCategory);
+
+        Usluga cinemaTicket = new Usluga("Кинотеатр","новые фильмы",
+                new BigDecimal(500),"12335647",cinemaCategory);
+
+        Usluga roamingPackage20GB = new Usluga("Пакет 20 ГБ", "new package",
+                BigDecimal.valueOf(1000), "12335647", roamingCategory );
+
+        Usluga internetPackage10GB = new Usluga("Пакет 10 ГБ","new package",
+               BigDecimal.valueOf(200), "12335647", internetCategory);
+        Usluga internetPackage30GB = new Usluga("Пакет 30 ГБ", "new package",
+                 BigDecimal.valueOf(550),"12335647", internetCategory);
 
         uslugaRepository.save(callPackage60);
-//        uslugaRepository.save(callBlackList);
-//        uslugaRepository.save(callPackage30);
-//        uslugaRepository.save(cinemaTicket);
-//        uslugaRepository.save(roamingPackage20GB);
-//        uslugaRepository.save(internetPackage10GB);
-//        uslugaRepository.save(internetPackage30GB);
+        uslugaRepository.save(callBlackList);
+        uslugaRepository.save(callPackage30);
+        uslugaRepository.save(cinemaTicket);
+        uslugaRepository.save(roamingPackage20GB);
+        uslugaRepository.save(internetPackage10GB);
+        uslugaRepository.save(internetPackage30GB);
 
     }
 
